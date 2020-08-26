136d4e76cfd42af045e808e322e6a66dfa84dc15
rit-upy1 <61547344+rit-upy1@users.noreply.github.com>
2020-08-26 15:02:27 +0530

    Create NumberOfPeopleInABus.java
    Pretty easy. In my opinion

---
+++ Lists/NumberOfPeopleInABus.java
@@ -0,0 +1,49 @@
+package Lists;
+
+import java.util.ArrayList;
+
+public class NumberOfPeopleInABus {
+/*
+There is a bus moving in the city, and it takes and drop some people in each bus stop.
+
+You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+
+Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+
+Take a look on the test cases.
+
+public class MetroTest {
+    Metro metro = new Metro();
+  
+    @Test
+    public void test1() {
+    ArrayList<int[]> list = new ArrayList<int[]>();
+    list.add(new int[] {10,0});
+    list.add(new int[] {3,5});
+    list.add(new int[] {2,5});
+    assertEquals(5, metro.countPassengers(list));
+    }
+    
+    
+}
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+
+The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+*/
+    public static void main(String[] args) {
+        ArrayList<int[]> list = new ArrayList<int[]>();
+        list.add(new int[] { 10, 0 });
+        list.add(new int[] { 3, 5 });
+        list.add(new int[] { 2, 5 });
+        System.out.println(countPassengers(list));
+    }
+    public static int countPassengers(ArrayList<int[]> stops) {
+        int peopleRemaining = 0;
+
+        for (int[] i : stops) {
+            peopleRemaining += i[0] - i[1];
+        }
+        return peopleRemaining;
+    }
+}
